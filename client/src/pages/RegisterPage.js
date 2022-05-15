517485395dcc22c83dba7fe762fd58f36677c88d
jylee9937 <lkj49864@ajou.ac.kr>
2022-05-15 17:46:58 +0900

    회원가입 디자인 변경

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,6 +1,43 @@
 import React, {useState} from 'react';
 import {useDispatch} from "react-redux";
 import {registerUser} from "../_actions/user_action";
+import styled from 'styled-components';
+
+const Div = styled.div`
+    display: 'flex';
+    justify-content: 'center';
+    align-items: 'center';
+    width: '100%';
+    height: '100vh';
+    flex-direction: column;
+`;
+const H2 = styled.h2`
+    text-align: center;
+`;
+const Form = styled.form`
+    display:flex;
+    flex-direction:column;
+`;
+
+const Input = styled.input`
+    width: 312px;
+    height: 48px;
+    font-size: 15px;
+    line-height: 1.47;
+    border: 1px solid #dee2e6;
+    border-radius: 4px;
+    padding: 13px 12px;
+    box-sizing: inherit;
+    margin-bottom: 12px;
+    padding-block: inherit;
+`;
+
+const Button = styled.button`
+    margin-top: 12px;
+    width: 338px;
+    height: 48px;
+    font-size: 15px;
+`;
 
 const RegisterPage = (props) => {
     const dispatch = useDispatch();
@@ -47,25 +84,23 @@ const RegisterPage = (props) => {
     }
 
     return (
-        <div style={{
-            display: 'flex', justifyContent: 'center', alignItems: 'center'
-            , width: '100%', height: '100vh'
-        }}>
-            <form style={{display:'flex', flexDirection:'column'}} onSubmit={onSubmitHandler}>
-                <label>Email</label>
-                <input type="email" value={Email} onChange={onEmailHandler}/>
-                <label>Name</label>
-                <input type="text" value={Name} onChange={onNameHandler}/>
-                <label>Password</label>
-                <input type="password" value={Password} onChange={onPasswordHandler}/>
-                <label>Confirm Password</label>
-                <input type="password" value={ConfirmPassword} onChange={onConfirmPasswordHandler}/>
-                <button>
+        <Div>
+            <H2>SLIRI</H2>
+            <Form onSubmit={onSubmitHandler}>
+                <label>이메일</label>
+                <Input placeholder="example@email.com" type="email" value={Email} onChange={onEmailHandler}/>
+                <label>이름</label>
+                <Input placeholder="홍길동" type="text" value={Name} onChange={onNameHandler}/>
+                <label>비밀번호</label>
+                <Input placeholder="******" type="password" value={Password} onChange={onPasswordHandler}/>
+                <label>비밀번호 확인</label>
+                <Input placeholder="******" type="password" value={ConfirmPassword} onChange={onConfirmPasswordHandler}/>
+                <Button>
                     Register
-                </button>
-            </form>
+                </Button>
+            </Form>
 
-        </div>
+        </Div>
     );
 };
 
