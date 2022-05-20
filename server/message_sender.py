e64feba67404c328fa5a64d188cf36ad74f74c12
Jay Kim <jay7491@naver.com>
2022-05-20 12:25:47 +0900

    main, audio_classifier, message_sender 업로드

---
+++ server/message_sender.py
@@ -0,0 +1,24 @@
+import requests
+import time
+
+myToken = "xoxb-3451298675446-3443353129815-ybjtXOw57L7B4O0vdslo1U4s"
+
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+                             headers={"Authorization": "Bearer " + token},
+                             data={"channel": channel, "text": text}
+                             )
+    print(response)
+
+def notice1():
+    for i in range(5):
+        post_message(myToken, "#project", "초인종이 울렸습니다!")
+
+        time.sleep(5)
+
+def notice2():
+    for i in range(15):
+        post_message(myToken, "#project", "화재경보가 울렸습니다!")
+
+        time.sleep(3)
+
